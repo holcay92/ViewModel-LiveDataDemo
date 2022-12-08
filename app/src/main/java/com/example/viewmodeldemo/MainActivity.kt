86c4ebaaf3db09ec6c869bc2b62590d35875d76f
halil.olcay <holcay92@gmail.com>
2022-12-09 01:58:29 +0300

    live data used on this app

--- app/src/main/java/com/example/viewmodeldemo/MainActivity.kt
+++ app/src/main/java/com/example/viewmodeldemo/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.viewmodeldemo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 
 class MainActivity : AppCompatActivity() {
@@ -16,13 +17,28 @@ class MainActivity : AppCompatActivity() {
         val counter = findViewById<TextView>(R.id.tt)
         val countButton = findViewById<TextView>(R.id.bt)
         //counter.text = count.toString()
-        counter.text = viewModel.count.toString()
+        // counter.text = viewModel.count.toString()
+
+        // this is how we observe the mutable live data
+        // we need to pass an observer to the mutable live data
+        // the observer is an interface that has a method called onChanged
+        // this method is called when the value of the mutable live data changes
+        // the onChanged method takes the new value of the mutable live data as a parameter
+        // the onChanged method is called on the main thread
+
+        viewModel.count.observe(this, Observer {
+            // "it" is the new value of the mutable live data
+            counter.text = it.toString()
+        })
 
         countButton.setOnClickListener {
-           // count++
-          //  counter.text = count.toString()
+            // count++
+            //  counter.text = count.toString()
+
+            // after observing the mutable live data we don't need to update the text view
             viewModel.updateCount()
-            counter.text = viewModel.count.toString()
+
+            //counter.text = viewModel.count.toString()
         }
     }
 }
--- app/src/main/java/com/example/viewmodeldemo/MainActivityViewModel.kt
+++ app/src/main/java/com/example/viewmodeldemo/MainActivityViewModel.kt
@@ -5,9 +5,16 @@ import androidx.lifecycle.ViewModel
 
 class MainActivityViewModel: ViewModel() {
    // var count = 0
+    // mutable live data is a live data that can be changed
     var count = MutableLiveData<Int>()
+    // we need to initialize the mutable live data
+    init {
+        count.value = 0
+    }
 
     fun updateCount() {
      //   ++count
+     // this is how we change the value of mutable live data
+    count.value = (count.value)?.plus(1)
     }
 }
